f928e49ba6129c4b42fdc51f0250762a172fe711
Vipin Gupta <vipin0900@gmail.com>
2020-12-09 07:57:05 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """Hukum mere aaka :joy:"""
+    START_TEXT = """Hukum mere aaka!"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
